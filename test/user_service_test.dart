b6ea2afb0ede2688b7d87965f8510f3be406f029
Thiha Thant Sin <42347119+ThihaThantSin777@users.noreply.github.com>
2025-08-09 18:08:04 +0700

    fix and add FakeUserRepo

--- test/user_service_test.dart
+++ test/user_service_test.dart
@@ -1,24 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:test_automation_demo/src/user_repo.dart';
 
-class MockUserRepo extends Mock implements UserRepo {}
+class FakeUserRepo implements UserRepo {
+  @override
+  Future<String> fetchUserName() async => 'Thiha';
+}
 
 void main() {
-  late MockUserRepo repo;
-  late UserService service;
-
-  setUp(() {
-    repo = MockUserRepo();
-    service = UserService(repo);
-  });
-
   test('greeting() uses repo name', () async {
-    when(() => repo.fetchUserName())
-        .thenAnswer((_) => Future.value('Thiha')); // explicit Future<String>
+    final service = UserService(FakeUserRepo());
 
     final text = await service.greeting();
+
     expect(text, 'Hello, Thiha!');
-    verify(() => repo.fetchUserName()).called(1);
   });
 }
